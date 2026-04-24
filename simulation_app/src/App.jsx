8959b9c58e104ecc2d2c29d62402b0531d015efb
BADR EDDINE OUAJIH <badreouaj@gmail.com>
2026-04-24 10:11:19 +0100

    vv2

--- simulation_app/src/App.jsx
+++ simulation_app/src/App.jsx
@@ -16,7 +16,7 @@ function CenterPanelContent({ avatarSpeaking, isAnalyzing, startLiveAnalysis }) 
   return (
     <>
       <div className="header-bar">
-        <h1>Neural Simulation Core - VVV</h1>
+        <h1>Neural Simulation Core - VVV2</h1>
       </div>
       <Avatar isSpeaking={avatarSpeaking || isAnalyzing} />
       <LiveDiagnosticBar isAnalyzing={isAnalyzing} startLiveAnalysis={startLiveAnalysis} />
